d758b1ca29dbd9aee99566f2af4849c714d03920
BWsix <itisbw62@gmail.com>
2022-07-24 12:04:27 +0800

    chore: update project title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>7-11 finder</title>
+        <meta name="title" content="7-11 finder" />
+        <meta
+          name="description"
+          content="7-11 finder會在google map中自動標出離你最近的7-11"
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
